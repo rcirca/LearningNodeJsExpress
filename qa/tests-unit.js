b804d55260ceaca1ec24c8e046130f0143ba9281
recheverria <failquality@gmail.com>
2017-11-24 14:32:31 -0800

    #fixed unit test to use assert instead of expect that was falsely returning true #Added unit tests for fake weather data

--- qa/tests-unit.js
+++ qa/tests-unit.js
@@ -1,8 +1,23 @@
-var lameFortune = require('../lib/lame-fortunes/lame-fortunes');
-var expect = require('chai').expect;
+var lameFortune = require('../lib/lame-fortunes/lame-fortunes')
+var fakeWeather = require('../lib/fake-weather/fake-weather');
+var chai = require('chai');
+
+var assert = chai.assert;
 
 suite('Lame fortunes', function () {
    test('getFortune() should return a fortune', function () {
-       expect(typeof lameFortune.getLameFortune() === 'string');
+       assert.strictEqual(typeof lameFortune.getLameFortune(), 'string', "return type is a string");
+   });
+});
+
+suite('Fake Weather', function () {
+   test('getWeather() should return an object with an array', function () {
+      var weather = fakeWeather.getWeatherData();
+      assert.isArray(weather.locations);
+   });
+
+   test('getWeather() locations should not be empty', function () {
+      var weather = fakeWeather.getWeatherData();
+      assert.isAbove(weather.locations.length,0);
    });
 });
